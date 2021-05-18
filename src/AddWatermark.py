9e62b00a5334799ec91b43409fe9061e7260319d
Szabi Szamel <szabi.szamel@pdfix.net>
2021-05-18 14:47:08 +0200

    SDK-604 Added AddWatermark sample for python examples, and PdfMatrix transfromations to Utils

--- src/AddWatermark.py
+++ src/AddWatermark.py
@@ -2,7 +2,7 @@
 # Example how to extract text from PDF.
 
 # import utils to load required shared libraries
-from Utils import inputPath, outputPath
+from Utils import *
 from Pdfix import *
 
 pdfix  = GetPdfix()
@@ -13,6 +13,96 @@ doc = pdfix.OpenDoc(inputPath + "/test.pdf", "")
 if doc is None:
     raise Exception('Unable to open pdf : ' + pdfix.GetError())
 
-# TODO:
+img_stm = pdfix.CreateFileStream( inputPath + "/watermark.png" , kPsReadOnly)
+if img_stm is None:
+    raise Exception(pdfix.GetError())
+
+# identify image format from file path
+format = kImageFormatPng
+
+image_obj = doc.CreateXObjectFromImage(img_stm, format)
+
+if image_obj is None:
+    raise Exception(pdfix.GetError())
+
+page_num = doc.GetNumPages()
+for i in range(page_num):
+    page = doc.AcquirePage(i)
+    if page is None:
+        raise Exception(pdfix.GetError())
+
+    content = page.GetContent()
+    if content is None:
+        raise Exception(pdfix.GetError())
+            
+    xobjdict = image_obj.GetStreamDict()
+    width = xobjdict.GetNumber("Width")
+    height = xobjdict.GetNumber("Height")
+
+    # prepare the matrix
+    page_mx = page.GetDefaultMatrix()
+    crop_rect = page.GetCropBox()
+    page_mx_rev = PdfMatrixInverse(page_mx)
+
+    # scale
+    scale = 1.5
+    width_scaled = width * scale
+    height_scaled = height * scale
+
+    matrix = PdfMatrix()
+    matrix.a = width_scaled
+    matrix.b = 0
+    matrix.c = 0
+    matrix.d = height_scaled
+    matrix.e = 0
+    matrix.f = 0
+
+    # rotation
+    rotation = 45.0
+    if rotation != 0.0:
+        matrix = PdfMatrixTranslate(matrix, -width_scaled / 2, -height_scaled / 2, False)
+        matrix = PdfMatrixRotate(matrix, (rotation / 180.0) * kPi, False)
+        matrix = PdfMatrixTranslate(matrix, width_scaled / 2, height_scaled / 2, False)
+    
+    rect_h = crop_rect.right - crop_rect.left
+    rect_v = crop_rect.top - crop_rect.bottom
+
+    # horizontal align
+    h_align = kAlignmentCenter
+    if (h_align == kAlignmentCenter):
+        matrix = PdfMatrixTranslate(matrix, (rect_h - width_scaled) / 2, 0.0, False)
+    elif (h_align == kAlignmentRight):
+        matrix = PdfMatrixTranslate(matrix, (rect_h - width_scaled), 0.0, False)
+
+    # vertical align
+    v_align = kAlignmentCenter
+    if (v_align == kAlignmentCenter):
+        matrix = PdfMatrixTranslate(matrix, 0.0, (rect_v - height_scaled) / 2, False)
+    elif (v_align == kAlignmentTop):
+        matrix = PdfMatrixTranslate(matrix, 0.0, (rect_v - height_scaled), False)
+
+    # horizontal and vertical offset
+    offs_h = 10
+    offs_v = 20
+
+    # -offs_v because y coordinate from top to bottom
+    matrix = PdfMatrixTranslate(matrix, offs_h, -offs_v, False)
+    order_top = 1
+    if order_top == 1:
+        position = -1
+    else:
+        position = 0
+    imageobject = content.AddNewImage(position, image_obj, matrix)
+
+    # set opacity of the image
+    opacity = 0.8
+    graphicState = imageobject.GetGState()
+    graphicState.color_state.fill_opacity = (int)(opacity * 255)
+    imageobject.SetGState(graphicState)
+    page.SetContent()
+    page.Release()
+
+if (not doc.Save(outputPath + "/AddWatermark.pdf", kSaveFull)):
+    raise Exception(pdfix.GetError())
 
 doc.Close()
--- src/Utils.py
+++ src/Utils.py
@@ -1,7 +1,7 @@
 # Utils.py
 # Pdfix utils
 
-import platform, os
+import platform, os, math
 from Pdfix import Pdfix_init
 from PdfToHtml import PdfToHtml_init
 from OcrTesseract import OcrTesseract_init
@@ -27,4 +27,54 @@ inputPath = basePath + "/../resources"
 outputPath = basePath + "/../output"
 if not os.path.isdir(outputPath): 
   os.mkdir(outputPath)
+
+kPi = 3.1415926535897932384626433832795
+
+def PdfMatrixConcat(m:PdfMatrix, m1:PdfMatrix, prepend:bool):
+  ret = PdfMatrix()
+  if (prepend):
+    swap = m
+    m = m1
+    m1 = swap
+  ret.a = m.a * m1.a + m.b * m1.c
+  ret.b = m.a * m1.b + m.b * m1.d
+  ret.c = m.c * m1.a + m.d * m1.c
+  ret.d = m.c * m1.b + m.d * m1.d
+  ret.e = m.e * m1.a + m.f * m1.c + m1.e
+  ret.f = m.e * m1.b + m.f * m1.d + m1.f
+  return ret
+
+def PdfMatrixRotate(m:PdfMatrix, radian:float, prepend:bool):
+  cosValue = math.cos(radian)
+  sinValue = math.sin(radian)
+  m1 = PdfMatrix()
+  m1.a = cosValue
+  m1.b = sinValue
+  m1.c = -sinValue
+  m1.d = cosValue
+  return PdfMatrixConcat(m, m1, prepend)
+
+def PdfMatrixTranslate(m:PdfMatrix, x:float, y:float, prepend:bool):
+  ret = m
+  if prepend:
+    ret.e = m.e + x * m.a + y + m.c
+    ret.f = m.f + y * m.d + x * m.b
+  ret.e = m.e + x
+  ret.f = m.f + y
+  return ret
+   
+
+def PdfMatrixInverse(orig:PdfMatrix):
+  inverse = PdfMatrix()
+  i = orig.a * orig.d - orig.b * orig.c
+  if abs(i) == 0:
+    return inverse
+  j = -i
+  inverse.a = orig.d / i
+  inverse.b = orig.b / j
+  inverse.c = orig.c / j
+  inverse.d = orig.a / i
+  inverse.e = (orig.c * orig.f - orig.d * orig.e) / i
+  inverse.f = (orig.a * orig.f - orig.b * orig.e) / j
+  return inverse
 
