5d85830e1ae0a1028d6ca28b5686eb76fc78cf59
jbaranec <jozef.baranec@gmail.com>
2021-10-29 18:25:12 +0200

    PDFix SDK 6.16.0

--- setup_python.sh
+++ setup_python.sh
@@ -5,8 +5,8 @@ pushd "$(dirname $0)"
 
 # download and unpack the pdfix sdk build
 
-SDK_VER=6.15.0
-SDK_BUILD=7f8dfd6d
+SDK_VER=6.16.0
+SDK_BUILD=810be50f
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   SDK_ZIP="pdfix_sdk_"$SDK_VER"_"$SDK_BUILD"_linux.tar.gz"
