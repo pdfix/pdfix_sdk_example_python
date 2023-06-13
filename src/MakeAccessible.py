df837609ba0a7c1f641e177e085b4694dcdbeb0d
jbaranec <jozef.baranec@gmail.com>
2023-06-13 21:53:23 +0200

    README.md and MakeAccseeible with preflight

--- src/MakeAccessible.py
+++ src/MakeAccessible.py
@@ -12,9 +12,17 @@ doc = pdfix.OpenDoc(inputPath + "/test.pdf", "")
 if doc is None:
     raise Exception('Unable to open pdf : ' + pdfix.GetError())
 
+# preflight document for better tagging structure (optional)
+tmpl = doc.GetTemplate()
+for i in range(0, doc.GetNumPages()):
+    tmpl.AddPage(i, 0, None)
+tmpl.Update(0, None)
+
+# set the document title and language
 title = "Document title"
 lang = "en-US"
 
+# run make accessibile
 accessibleParams = PdfAccessibleParams()
 if not doc.MakeAccessible(accessibleParams, title, lang, 0, None):
     raise Exception(pdfix.GetError())
