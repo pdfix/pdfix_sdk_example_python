64334f282d338c1cd1eef7b67badcb60a6f7aef2
jbaranec <jozef.baranec@gmail.com>
2024-05-02 21:16:27 +0200

    make-accessible example with PsCommand

--- src/MakeAccessible.py
+++ src/MakeAccessible.py
@@ -12,61 +12,18 @@ doc = pdfix.OpenDoc(inputPath + "/test.pdf", "")
 if doc is None:
     raise Exception('Unable to open pdf : ' + pdfix.GetError())
 
-# preflight document for better tagging structure (optional)
-tmpl = doc.GetTemplate()
-for i in range(0, doc.GetNumPages()):
-    tmpl.AddPage(i)
-tmpl.Update()
+cmd = doc.GetCommand()
 
-# set the document title and language
-title = "Document title"
-lang = "en-US"
+stm = pdfix.CreateFileStream(inputPath + "/make-accessible.json", kPsReadOnly)
+if not stm:
+    raise Exception(pdfix.GetError())
 
-# run make accessibile
-# single MakeAccessible method has been deprecated. You should call individual methods fixing
-# problems with document acessibility. The set of methods in this example should be customized 
-# for the needs of the document
+if not cmd.LoadParamsFromStream(stm, kDataFormatJson):
+    raise Exception(pdfix.GetError())
+stm.Destroy()
 
-# clear document structure
-doc.RemoveTags()
-
-# embed fonts
-doc.EmbedFonts(True)
-
-# add missing unicode
-doc.AddFontMissingUnicode()
-
-# prepare template for document preflight
-tmpl = doc.GetTemplate()
-
-for i in range(doc.GetNumPages()):
-    page = doc.AcquirePage(i)
-
-    # flatten form XObjects 
-    page.FlattenFormXObjects()
-
-    # add page to template process
-    tmpl.AddPage(i)
-
-    page.Release()
-
-# run preflight on the document template
-tmpl.Update()
-
-# add tags
-tagParams = PdfTagsParams()
-doc.AddTags(tagParams)
-
-# set language
-doc.SetLang("en")
-
-# set doc ua part
-doc.SetPdfStandard(kPdfStandardPdfUA, "1")
-
-# create bookmarkd
-doc.CreateBookmarks()
-
-# other accessibility fixes are available using commands. see user guide for reference
+if not cmd.Run():
+    raise Exception(pdfix.GetError())
 
 if not doc.Save(outputPath + "/MakeAccessible.pdf", kSaveFull):
     raise Exception(pdfix.GetError())
