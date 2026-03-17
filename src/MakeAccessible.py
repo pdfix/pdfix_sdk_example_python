226164d3eeebf33b667d1af3c3f581792490dd54
Jozef Baranec <jozef.baranec@gmail.com>
2026-03-17 18:45:56 +0100

    version update for PDFix SDK 8.7.16

--- src/MakeAccessible.py
+++ src/MakeAccessible.py
@@ -1,43 +1,90 @@
-# MakeAccessible.py
-
 # import utils to load required shared libraries
+import json
+
 from Utils import inputPath, outputPath
 from pdfixsdk import *
 
-commandPath = ""#inputPath + "/make-accessible.json"
+import Utils
 
-pdfix  = GetPdfix()
+commandPath = ""  # inputPath + "/make-accessible.json"
+
+pdfix = GetPdfix()
 if pdfix is None:
-    raise Exception('Pdfix Initialization fail')
+    raise Exception("Pdfix Initialization fail")
 
 doc = pdfix.OpenDoc(inputPath + "/test.pdf", "")
 if doc is None:
-    raise Exception('Unable to open pdf : ' + pdfix.GetError())
+    raise Exception("Unable to open pdf : " + pdfix.GetError())
 
 command = doc.GetCommand()
+if command is None:
+    raise Exception(pdfix.GetError())
 
 cmdStm = None
 
-# load the make-accessible command from JSON or use the default
-if commandPath == "":
-    cmdStm = pdfix.CreateMemStream()
-    if not command.SaveCommandsToStream(kActionMakeAccessible, cmdStm, kDataFormatJson, kSaveFull):
+
+def extract_json_name(json_text):
+    if not json_text:
+        return None
+
+    try:
+        data = json.loads(json_text)
+        return data.get("name")
+    except Exception:
+        return None
+
+
+try:
+    # load the make_accessible command from JSON file
+    # or find the embedded custom action named "make_accessible"
+    if commandPath == "":
+        cmd_count = command.GetNumCustomActions()
+
+        for i in range(cmd_count):
+            tmpStm = pdfix.CreateMemStream()
+            if tmpStm is None:
+                raise Exception(pdfix.GetError())
+
+            try:
+                if not command.SaveCustomActionToStream(
+                    i, tmpStm, kDataFormatJson, kSaveFull
+                ):
+                    raise Exception(pdfix.GetError())
+
+                json_text = bytearray(Utils.stream_to_data(tmpStm))
+                name = extract_json_name(json_text)
+
+                if name == "make_accessible":
+                    cmdStm = tmpStm
+                    tmpStm = None
+                    break
+            except Exception as e:
+                print("[WARNING]: Loading JSON command failed. [{}] {}".format(i, e))
+            finally:
+                if tmpStm is not None:
+                    tmpStm.Destroy()
+
+        if cmdStm is None:
+            raise Exception("Embedded custom action 'make_accessible' was not found.")
+    else:
+        cmdStm = pdfix.CreateFileStream(commandPath, kPsReadOnly)
+        if cmdStm is None:
+            raise Exception(pdfix.GetError())
+
+    if not command.LoadParamsFromStream(cmdStm, kDataFormatJson):
         raise Exception(pdfix.GetError())
-else:
-    cmdStm = pdfix.CreateFileStream(commandPath, kPsReadOnly)
-    if not cmdStm:
+
+    cmdStm.Destroy()
+    cmdStm = None
+
+    # run the command
+    if not command.Run():
         raise Exception(pdfix.GetError())
 
-if not command.LoadParamsFromStream(cmdStm, kDataFormatJson):
-    raise Exception(pdfix.GetError())
+    if not doc.Save(outputPath + "/MakeAccessible.pdf", kSaveFull):
+        raise Exception(pdfix.GetError())
 
-cmdStm.Destroy()
-
-# run the command
-if not command.Run():
-    raise Exception(pdfix.GetError())
-
-if not doc.Save(outputPath + "/MakeAccessible.pdf", kSaveFull):
-    raise Exception(pdfix.GetError())
-
-doc.Close()
+finally:
+    if cmdStm is not None:
+        cmdStm.Destroy()
+    doc.Close()
